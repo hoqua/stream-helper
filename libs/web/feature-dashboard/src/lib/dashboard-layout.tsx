226ead48eaa43e0d36e7f6bc178b210b6e27bfdd
MikeXado <mike.gurin21@gmail.com>
2025-09-01 16:17:22 +0300

    get projects

--- libs/web/feature-dashboard/src/lib/dashboard-layout.tsx
+++ libs/web/feature-dashboard/src/lib/dashboard-layout.tsx
@@ -1,6 +1,6 @@
 import { ReactNode } from 'react';
-import { mockProjects } from './components/constants';
 import Navbar from './components/navbar';
+import { getProjects } from './loader';
 
 export async function DashboardLayout({
   projectId,
@@ -9,12 +9,12 @@ export async function DashboardLayout({
   projectId: string;
   children: ReactNode;
 }) {
-  //TODO: GET USER PROJECTS HERE
+  const projects = await getProjects();
   return (
     <div className="min-h-screen bg-gradient-to-br from-gray-900 via-black to-gray-900">
       <div className="absolute inset-0 bg-grid-white/[0.02] -z-10" />
 
-      <Navbar projects={mockProjects} selectedProjectId={projectId} />
+      <Navbar projects={projects} selectedProjectId={projectId} />
 
       <div className="max-w-7xl mx-auto px-4 py-8">{children}</div>
     </div>
