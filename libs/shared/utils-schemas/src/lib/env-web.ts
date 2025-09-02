7871fe272046e91ac7e3ecbc2502e8251e825f10
Dmitrii Zolotuhin <dmitriizolotuhin@cloud.upwork.com>
2025-09-02 11:02:27 +0300

    feat/project-name-env

--- libs/shared/utils-schemas/src/lib/env-web.ts
+++ libs/shared/utils-schemas/src/lib/env-web.ts
@@ -4,8 +4,8 @@ import { z } from 'zod';
 export const envWeb = createEnv({
   server: {
     DATABASE_URL: z.string().url(),
-    VERCEL_CLIENT_ID: z.string().min(1),
-    VERCEL_CLIENT_SECRET: z.string().min(1),
+    CLIENT_ID: z.string().min(1),
+    CLIENT_SECRET: z.string().min(1),
     NODE_ENV: z.enum(['development', 'production', 'test']).default('development'),
   },
 
@@ -15,11 +15,11 @@ export const envWeb = createEnv({
 
   runtimeEnv: {
     DATABASE_URL: process.env.DATABASE_URL,
-    VERCEL_CLIENT_ID: process.env.VERCEL_CLIENT_ID || process.env.CLIENT_ID,
-    VERCEL_CLIENT_SECRET: process.env.VERCEL_CLIENT_SECRET || process.env.CLIENT_SECRET,
+    CLIENT_ID: process.env.CLIENT_ID,
+    CLIENT_SECRET: process.env.CLIENT_SECRET,
     NODE_ENV: process.env.NODE_ENV,
     NEXT_PUBLIC_VERCEL_URL: process.env.NEXT_PUBLIC_VERCEL_URL,
   },
 
-  skipValidation: !!process.env.SKIP_ENV_VALIDATION
+  skipValidation: !!process.env.SKIP_ENV_VALIDATION,
 });
