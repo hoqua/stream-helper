d790f3a72173c772d973fa9f77b646c10de8f5d9
Dmitrii Zolotuhin <dmitriizolotuhin@cloud.upwork.com>
2025-09-02 17:50:42 +0300

    comment

--- libs/shared/data-access-db/src/lib/client.ts
+++ libs/shared/data-access-db/src/lib/client.ts
@@ -2,6 +2,7 @@ import { neon } from '@neondatabase/serverless';
 import { drizzle } from 'drizzle-orm/neon-http';
 import * as schema from './schema';
 
+// NOTE: env-web and env-api are intentionally NOT used here. Used for all apps and validation not possible
 const sql = neon(process.env.DATABASE_URL!);
 
 export const db = drizzle(sql, { schema });
