1258adefe02040ecab4b2efda012981ae15f6e0d
MikeXado <mike.gurin21@gmail.com>
2025-09-02 19:27:32 +0300

    fix typecheck issue

--- libs/shared/data-access-db/src/lib/queries/user.ts
+++ libs/shared/data-access-db/src/lib/queries/user.ts
@@ -1,14 +1,9 @@
 import { eq } from 'drizzle-orm';
 import { db } from '../client';
 import { NewUser, users } from '../schema';
-import { ConfigurationType } from '@durablr/shared-utils-schemas';
 
 export async function createUser(data: NewUser) {
   return await db.insert(users).values(data).returning();
-}
-
-export async function configureUser(id: string, data: ConfigurationType) {
-  return await db.update(users).set(data).where(eq(users.configurationId, id));
 }
 
 export async function getAllUsers() {
