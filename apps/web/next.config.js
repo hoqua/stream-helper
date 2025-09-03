5bad1e0924c8134fe0eb0883f820999ad04972ae
Dmitrii Zolotuhin <dmitriizolotuhin@cloud.upwork.com>
2025-09-03 16:21:32 +0300

    ok roll back your sync

--- apps/web/next.config.js
+++ apps/web/next.config.js
@@ -5,13 +5,6 @@ const { composePlugins, withNx } = require('@nx/next');
 
 // Log environment variables during build
 console.log(process.env)
-console.log('🔍 VERCEL BUILD ENV VARS:');
-console.log('API_URL:', process.env.API_URL);
-console.log('NEXT_PUBLIC_API_URL:', process.env.NEXT_PUBLIC_API_URL);
-console.log('NODE_ENV:', process.env.NODE_ENV);
-console.log('VERCEL_ENV:', process.env.VERCEL_ENV);
-console.log('VERCEL_GIT_COMMIT_REF:', process.env.VERCEL_GIT_COMMIT_REF);
-console.log('VERCEL_URL:', process.env.VERCEL_URL);
 
 /**
  * @type {import('@nx/next/plugins/with-nx').WithNxOptions}
