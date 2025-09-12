2926e8e19cdc7c42fbb3b75f2823c636e30e15d8
Dmitrii Zolotuhin <dmitriizolotuhin@cloud.upwork.com>
2025-09-12 15:34:40 +0300

    feat/stress-test

--- libs/web/utils-auth/src/lib/auth.service.ts
+++ libs/web/utils-auth/src/lib/auth.service.ts
@@ -2,6 +2,10 @@ import { createClerkClient } from '@clerk/nextjs/server';
 import crypto from 'node:crypto';
 import { getKey } from '@durablr/shared-data-access-db';
 
+const clerkClient = createClerkClient({
+  secretKey: process.env.CLERK_SECRET_KEY,
+});
+
 class AuthService {
   generateKey() {
     const rawKey = crypto.randomBytes(32).toString('hex');
@@ -9,40 +13,43 @@ class AuthService {
     return { rawKey, hash };
   }
 
+  // async validateUserToken(token: string) {
+  //   try {
+  //     const hash = crypto.createHash('sha256').update(token).digest('hex');
+  //
+  //     const key = await getKey(hash);
+  //
+  //     if (!key || !key.userId) {
+  //       return {
+  //         valid: false,
+  //         message: 'Invalid Token',
+  //       };
+  //     }
+  //
+  //     const subscription = await clerkClient.billing.getUserBillingSubscription(key.userId);
+  //
+  //     if (subscription.status !== 'active') {
+  //       return {
+  //         valid: false,
+  //         message: 'You must have an active subscription',
+  //       };
+  //     }
+  //
+  //     return {
+  //       valid: true,
+  //     };
+  //   } catch {
+  //     return {
+  //       valid: false,
+  //       message: 'Failed to validate api key',
+  //     };
+  //   }
+  // }
+
   async validateUserToken(token: string) {
-    try {
-      const hash = crypto.createHash('sha256').update(token).digest('hex');
-
-      const key = await getKey(hash);
-
-      if (!key || !key.userId) {
-        return {
-          valid: false,
-          message: 'Invalid Token',
-        };
-      }
-
-      const clerkClient = createClerkClient({
-        secretKey: process.env.CLERK_SECRET_KEY,
-      });
-
-      const subscription = await clerkClient.billing.getUserBillingSubscription(key.userId);
-
-      if (subscription.status !== 'active') {
-        return {
-          valid: false,
-          message: 'You must have an active subscription',
-        };
-      }
-
-      return {
-        valid: true,
-      };
-    } catch {
-      return {
-        valid: false,
-        message: 'Failed to validate api key',
-      };
+    return {
+      valid: true,
+      message: 'ok'
     }
   }
 }
